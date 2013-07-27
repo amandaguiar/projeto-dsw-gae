a77da374da3c01583867791ec330a6c4863acb25
amandinhamotta@gmail.com <amandinhamotta@gmail.com@42266545-7730-36ad-c37c-393a95bfe889>
2013-07-27 05:00:18 +0000

    Criação das classes Componente, Sistema, StatusTicket e Ticket.

--- trunk/src/br/unirio/projetodswgae/model/Sistema.java
+++ trunk/src/br/unirio/projetodswgae/model/Sistema.java
@@ -1,5 +1,27 @@
 package br.unirio.projetodswgae.model;
 
-public class Sistema {
+import br.unirio.simplemvc.gae.datastore.DataObject;
 
+public class Sistema implements DataObject{
+
+	private int id;
+	private String nome;
+	
+	@Override
+	public int getId() {
+		return id;
+	}
+	@Override
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
 }
--- trunk/src/br/unirio/projetodswgae/model/Ticket.java
+++ trunk/src/br/unirio/projetodswgae/model/Ticket.java
@@ -1,5 +1,70 @@
 package br.unirio.projetodswgae.model;
 
-public class Ticket {
+import br.unirio.simplemvc.gae.datastore.DataObject;
 
+public class Ticket implements DataObject{
+
+	private int id;
+	private String identificador;
+	private String titulo;
+	private Sistema sistema;
+	private Componente componente;
+	private String descricao;
+	private StatusTicket status;
+	private Usuario operadorResponsavel;
+	
+	
+	@Override
+	public int getId() {
+		return id;
+	}
+	@Override
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getIdentificador() {
+		return identificador;
+	}
+	public void setIdentificador(String identificador) {
+		this.identificador = identificador;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public Sistema getSistema() {
+		return sistema;
+	}
+	public void setSistema(Sistema sistema) {
+		this.sistema = sistema;
+	}
+	public Componente getComponente() {
+		return componente;
+	}
+	public void setComponente(Componente componente) {
+		this.componente = componente;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public StatusTicket getStatus() {
+		return status;
+	}
+	public void setStatus(StatusTicket status) {
+		this.status = status;
+	}
+	public Usuario getOperadorResponsavel() {
+		return operadorResponsavel;
+	}
+	public void setOperadorResponsavel(Usuario operadorResponsavel) {
+		this.operadorResponsavel = operadorResponsavel;
+	}
+	
+	
+	
 }
