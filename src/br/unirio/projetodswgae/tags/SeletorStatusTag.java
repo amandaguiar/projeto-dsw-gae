3fa2f61cf2c8689bb8ef6d2f8eee673cb3d1dedd
jeanfreitas01@gmail.com <jeanfreitas01@gmail.com@42266545-7730-36ad-c37c-393a95bfe889>
2013-08-04 07:07:41 +0000

    err

--- src/br/unirio/projetodswgae/tags/SeletorStatusTag.java
+++ src/br/unirio/projetodswgae/tags/SeletorStatusTag.java
@@ -77,49 +77,58 @@ public class SeletorStatusTag extends TagSupport
 		
 		try
 		{
-			out.write("<select name='" + StringEscapeUtils.escapeHtml4(id) + "' size='1' id='" + StringEscapeUtils.escapeHtml4(id) + "'");
-
-			if (estilo.length() > 0)
-				out.write(" style='" + StringEscapeUtils.escapeHtml4(estilo) + "'");
-
-			if (classe.length() > 0)
-				out.write(" class='" + StringEscapeUtils.escapeHtml4(classe) + "'");
-
-			out.write(">\n");
-
-			List<String> statusTickets = new ArrayList<String>();
-			statusTickets.add(StatusTicket.NOVO.getCodigo());
-			/* Verifica se o usuario escolheu criar um novo ticket */
-			if(idTicket.equalsIgnoreCase("-1")){
+			if(idTicket.equals("-1")){
+				out.write("<select disabled name='" + StringEscapeUtils.escapeHtml4(id) + "' size='1' id='" + StringEscapeUtils.escapeHtml4(id) + "'");
 				String codigo = StatusTicket.NOVO.getCodigo();
 				String selecionado = " SELECTED";
 				out.write("<option value='" + codigo + "'" + selecionado + ">" + StringEscapeUtils.escapeHtml4(codigo) + "</option>\n");
+				out.write("</select>\n");
 			}
-			
-			/* Verifica se o usuario escolheu editar um ticket existente */
 			else{
-				if(tipoUsuario.equalsIgnoreCase(TipoUsuario.OPERADOR.getNome())){
-					statusTickets.add(StatusTicket.RESOLVIDO.getCodigo());
-					statusTickets.add(StatusTicket.INVALIDADO.getCodigo());
-				}
-				else if(tipoUsuario.equalsIgnoreCase(TipoUsuario.USUARIO_FINAL.getNome()) &&
-						!valor.equalsIgnoreCase(StatusTicket.NOVO.getCodigo())){
-					statusTickets.add(StatusTicket.REABERTO.getCodigo());
-					statusTickets.add(StatusTicket.FECHADO.getCodigo());
-				}
-				else if(tipoUsuario.equalsIgnoreCase(TipoUsuario.ADMINISTRADOR.getNome())){
-					statusTickets.add(StatusTicket.RESOLVIDO.getCodigo());
-					statusTickets.add(StatusTicket.INVALIDADO.getCodigo());
-					statusTickets.add(StatusTicket.REABERTO.getCodigo());
-					statusTickets.add(StatusTicket.FECHADO.getCodigo());
-				}
-				for (String codigo : statusTickets)
-				{
-					String selecionado = (valor.compareToIgnoreCase(codigo) == 0) ? " SELECTED" : "";
+				out.write("<select name='" + StringEscapeUtils.escapeHtml4(id) + "' size='1' id='" + StringEscapeUtils.escapeHtml4(id) + "'");
+
+				if (estilo.length() > 0)
+					out.write(" style='" + StringEscapeUtils.escapeHtml4(estilo) + "'");
+	
+				if (classe.length() > 0)
+					out.write(" class='" + StringEscapeUtils.escapeHtml4(classe) + "'");
+	
+				out.write(">\n");
+	
+				List<String> statusTickets = new ArrayList<String>();
+				statusTickets.add(StatusTicket.NOVO.getCodigo());
+				/* Verifica se o usuario escolheu criar um novo ticket */
+				if(idTicket.equalsIgnoreCase("-1")){
+					String codigo = StatusTicket.NOVO.getCodigo();
+					String selecionado = " SELECTED";
 					out.write("<option value='" + codigo + "'" + selecionado + ">" + StringEscapeUtils.escapeHtml4(codigo) + "</option>\n");
 				}
+				
+				/* Verifica se o usuario escolheu editar um ticket existente */
+				else{
+					if(tipoUsuario.equalsIgnoreCase(TipoUsuario.OPERADOR.getNome())){
+						statusTickets.add(StatusTicket.RESOLVIDO.getCodigo());
+						statusTickets.add(StatusTicket.INVALIDADO.getCodigo());
+					}
+					else if(tipoUsuario.equalsIgnoreCase(TipoUsuario.USUARIO_FINAL.getNome()) &&
+							!valor.equalsIgnoreCase(StatusTicket.NOVO.getCodigo())){
+						statusTickets.add(StatusTicket.REABERTO.getCodigo());
+						statusTickets.add(StatusTicket.FECHADO.getCodigo());
+					}
+					else if(tipoUsuario.equalsIgnoreCase(TipoUsuario.ADMINISTRADOR.getNome())){
+						statusTickets.add(StatusTicket.RESOLVIDO.getCodigo());
+						statusTickets.add(StatusTicket.INVALIDADO.getCodigo());
+						statusTickets.add(StatusTicket.REABERTO.getCodigo());
+						statusTickets.add(StatusTicket.FECHADO.getCodigo());
+					}
+					for (String codigo : statusTickets)
+					{
+						String selecionado = (valor.compareToIgnoreCase(codigo) == 0) ? " SELECTED" : "";
+						out.write("<option value='" + codigo + "'" + selecionado + ">" + StringEscapeUtils.escapeHtml4(codigo) + "</option>\n");
+					}
+				}
+				out.write("</select>\n");
 			}
-			out.write("</select>\n");
 		} catch (IOException e)
 		{
 			throw new JspException(e.getMessage());
